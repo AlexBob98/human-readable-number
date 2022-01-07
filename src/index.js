65bb7948501211971279d54123dbeb0032db95e5
Alexander Bobryshev <94990187+AlexBob98@users.noreply.github.com>
2022-01-07 21:17:49 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+  const numOne = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  const numTwo = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+  if (number < 20) return numOne[number]
+
+  if (number < 100 && number >= 20) {
+      if (number % 10 === 0) return numTwo[number / 10]
+
+      if (number % 10) {
+          return  numTwo[Math.floor(number / 10)] + ' ' + numOne[number % 10];
+      }
+  }
+
+  if (number >= 100) {
+      if (number % 100 === 0) return  numOne[number / 100] + ' hundred'
+
+      if (number % 100 && number % 100 < 20) {
+          return  numOne[Math.floor(number / 100)] + ' hundred ' + numOne[number % 100]; 
+      }
+
+      if (number % 100 && number % 100 >= 20) {
+          return number % 10 === 0
+              ? numOne[Math.floor(number / 100)] + ' hundred ' + numTwo[Math.floor((number % 100) / 10)]
+              : numOne[Math.floor(number / 100)] + ' hundred ' + numTwo[Math.floor((number % 100) / 10)] + ' ' + numOne[number % 10]
+      }
+     
+  }
 }
